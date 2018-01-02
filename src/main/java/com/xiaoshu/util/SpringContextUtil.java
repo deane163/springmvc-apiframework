59e29123f8859a1e4d0a27dcb5b7f9090b65ef1b
deane163 <deane163@126.com>
2018-01-02 20:44:39 +0800

    【功能】添加容器实现的工具类

---
+++ src/main/java/com/xiaoshu/util/SpringContextUtil.java
@@ -0,0 +1,61 @@
+package com.xiaoshu.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * 
+ * code is far away from bug with the animal protecting
+ * ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ * 　　┃　　　┃神兽保佑
+ * 　　┃　　　┃代码无BUG！
+ * 　　┃　　　┗━━━┓
+ * 　　┃　　　　　　　┣┓
+ * 　　┃　　　　　　　┏┛
+ * 　　┗┓┓┏━┳┓┏┛
+ * 　　　┃┫┫　┃┫┫
+ * 　　　┗┻┛　┗┻┛
+ *
+ *
+ * @Description : ApplicationContext 工具类实现
+ * ---------------------------------
+ * @Author : deane.administrator
+ * @Date : Create in 2018年1月2日下午8:41:48
+ * 
+ * Copyright (C)2013-2018 小树盛凯科技 All rights reserved.
+ */
+public class SpringContextUtil implements ApplicationContextAware {
+
+	private static ApplicationContext myApplicationContext;
+	
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext)
+			throws BeansException {
+		// 获得容器的上下文
+		myApplicationContext = applicationContext;
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public static <T> T getBean(String beanName){
+		return (T)myApplicationContext.getBean(beanName);
+	}
+	
+	
+	public static <T> T getBean(Class<T> requiredClass){
+		return myApplicationContext.getBean(requiredClass);
+	}
+
+	public static <T> T getBean(Class<T> requiredClass, Object... objects ){
+		return myApplicationContext.getBean(requiredClass, objects);
+	}
+}
